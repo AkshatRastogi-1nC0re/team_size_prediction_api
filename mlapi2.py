3644559f4856e5534144d5679bc3ac7beac50605
AKSHAT RASTOGI <akshat28vivek@hotmail.com>
2023-06-24 14:43:33 +0530

    inital commit

---
+++ mlapi2.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import pandas as pd
+
+app = FastAPI()
+
+class ScoringItem(BaseModel):
+    Class: int
+    TES: int
+    TIS: int
+    CLE: int
+    PSS: int
+    PCS: int
+    HC: int
+    RAS: int
+
+with open('best_model_team_rf.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.get("/")
+async def scoring_endpoint(item: ScoringItem):
+    df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
+    yhat = model.predict(df)
+    return {"prediction" : str(yhat)}
